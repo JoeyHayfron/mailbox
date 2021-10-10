5a340ac2c1a6d4f654c9b319ca42aacadba06cc8
JoeyHayfron <joeacq81@gmail.com>
2021-10-10 22:08:31 +0000

    Initial commit

--- scripts/installHooks.bash
+++ scripts/installHooks.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 GIT_DIR=$(git rev-parse --git-dir)
 echo "Installing hooks"
-ln -s ./preCommit.bash $GIT_DIR/hooks/pre-commit
+ln -s ../../scripts/preCommit.bash $GIT_DIR/hooks/pre-commit
 echo "Done!"
